404ee6bc5187e825990d234b9eed94abb5521a0b
Anton Firszov <antonfir@gmail.com>
2018-07-12 00:08:07 +0200

    AsyncWCF

---
+++ AsyncWCF/Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.ServiceReferences;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestServiceClient client = new TestServiceClient("NetTcpBinding_ITestService");
+            Console.WriteLine(client.Ping());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AsyncWCF/Server/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var svcHost = new ServiceHost(typeof(TestService));
+            Console.WriteLine("Available Endpoints :\n");
+            svcHost.Description.Endpoints.ToList().ForEach(endpoints => Console.WriteLine(endpoints.Address.ToString()));
+            svcHost.Open();
+            Console.ReadLine();
+        }
+    }
+}
